d4775e674a8a70504fc7a42f7f3b619531d27464
Sandy <sandysouzasantos@gmail.com>
2017-06-29 13:45:48 -0300

    Optimized main.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 var gulp = require('gulp'),
-    $ = require('gulp-load-plugins')();
+    imagemin = require('gulp-imagemin'),
+    uglify = require('gulp-uglify'),
+    minifyCSS = require('gulp-minify-css'),
+    minifyHTML = require('gulp-htmlmin');
 
 gulp.task('scripts', function () {
     gulp.src('js/*.js')
-        .pipe($.uglify())
+        .pipe(uglify())
         .pipe(gulp.dest('dist/js'));
 });
 
 gulp.task('styles', function () {
     gulp.src('css/*.css')
-        .pipe($.cssmin())
+        .pipe(minifyCSS())
         .pipe(gulp.dest('dist/css'));
 });
 
 gulp.task('minimage', function () {
     gulp.src('img/*.{png,jpg}')
-        .pipe($.imagemin())
-        .pipe(gulp.dest('dist/img'));
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task("htmlmin", function () {
     gulp.src('index.html')
-        .pipe($.htmlmin({collapseWhitespace: true}))
+        .pipe(minifyHTML({collapseWhitespace: true}))
         .pipe(gulp.dest('dist'));
 });
